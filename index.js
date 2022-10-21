f8cd3dcfbf7bec0c582b89d383fdfd63f9b18f6e
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-10-21 20:47:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastFavoriteCustomer = ""
+
+function upperCaseCustomerName() {
+
+    return customerName = customerName.toUpperCase()
+    
+}
+
+
+
+var bestCustomer; 
+function setBestCustomer() {
+      bestCustomer = 'not bob'
+    return bestCustomer;
+   
+}
+
+
+function overwriteBestCustomer(){
+   return bestCustomer = "maybe bob"
+}
+
+
+function changeLeastFavoriteCustomer() {
+  return   leastFavoriteCustomer = "here is bob "
+}
+
